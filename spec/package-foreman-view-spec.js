609fe225116d54af73cdb2fb1e329c20b2fbaed6
joshuaegclark <joshuaegclark@gmail.com>
2016-03-30 12:13:28 -0600

    init

---
+++ spec/package-foreman-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import PackageForemanView from '../lib/package-foreman-view';
+
+describe('PackageForemanView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
